6d07b3aa00011d563a2ff3602645fc43a75dff27
Ruchita Kalantri <rkalantri.kalantri@yahoo.com>
2021-08-08 11:11:39 +0530

    storing file data into database

--- controllers/admin.js
+++ controllers/admin.js
@@ -17,10 +17,28 @@ exports.getAddProduct = (req, res, next) => {
 
 exports.postAddProduct = (req, res, next) => {
   const title = req.body.title;
-  const imageUrl = req.file;
+  const image = req.file;
   const price = req.body.price;
   const description = req.body.description;
   const errors = validationResult(req);
+
+  if(!image) {
+    return res.status(422).render('admin/edit-product', {
+      pageTitle: 'Add Product',
+      path: '/admin/add-product',
+      editing: false,
+      hasError : true,
+      product: {
+        title: title,
+        price : price ,
+        description : description
+      },
+      errorMessage : 'Attached file is not an image' ,
+      validationErrors : []
+    }); 
+  }
+
+  const imageUrl = image.path;
 
   if(!errors.isEmpty) {
     return res.status(422).render('admin/edit-product', {
@@ -92,7 +110,7 @@ exports.postEditProduct = (req, res, next) => {
   const prodId = req.body.productId;
   const updatedTitle = req.body.title;
   const updatedPrice = req.body.price;
-  const updatedImageUrl = req.body.imageUrl;
+  const image = req.file;
   const updatedDesc = req.body.description;
 
   const errors = validationResult(req);
@@ -105,7 +123,6 @@ exports.postEditProduct = (req, res, next) => {
       hasError : true,
       product: {
         title: updatedTitle,
-        imageUrl : updatedImageUrl ,
         price : updatedPrice ,
         description : updatedDesc,
         _id : prodId
@@ -124,7 +141,10 @@ exports.postEditProduct = (req, res, next) => {
       product.title = updatedTitle;
       product.price = updatedPrice;
       product.description = updatedDesc;
-      product.imageUrl = updatedImageUrl;
+      if (image) {
+        product.imageUrl = image.path;
+      }
+      
       return product
         .save()
         .then(result => {
--- routes/admin.js
+++ routes/admin.js
@@ -42,8 +42,6 @@ router.post(
             .isString()
             .isLength({min : 3})
             .trim() ,
-        body('imageUrl')
-            .isURL() ,
         body('price')
             .isFloat() ,
         body('description')
