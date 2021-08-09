167cca834842198c0a45df2131d7b61a4f3cb99d
Ruchita Kalantri <rkalantri.kalantri@yahoo.com>
2021-08-09 12:43:18 +0530

    re-useing pagination logic

--- controllers/shop.js
+++ controllers/shop.js
@@ -9,13 +9,28 @@ const Order = require('../models/order');
 const ITEMS_PER_PAGE = 2;
 
 exports.getProducts = (req, res, next) => {
+  const page = +req.query.page || 1;
+  let totalItems;
+
   Product.find()
+    .countDocuments()
+    .then(numProducts => {
+      totalItems = numProducts;
+      return Product.find()
+        .skip((page - 1) * ITEMS_PER_PAGE)
+        .limit(ITEMS_PER_PAGE);
+    })
     .then(products => {
-      console.log(products);
       res.render('shop/product-list', {
         prods: products,
-        pageTitle: 'All Products',
-        path: '/products'
+        pageTitle: 'Products',
+        path: '/products',
+        currentPage: page,
+        hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+        hasPreviousPage: page > 1,
+        nextPage: page + 1,
+        previousPage: page - 1,
+        lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
       });
     })
     .catch(err => {
@@ -24,6 +39,7 @@ exports.getProducts = (req, res, next) => {
       return next(error);
     });
 };
+
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
@@ -41,38 +57,36 @@ exports.getProduct = (req, res, next) => {
       return next(error);
     });
 };
-
 exports.getIndex = (req, res, next) => {
   const page = +req.query.page || 1;
-  let totalItems ;
+  let totalItems;
 
-  Product
-    .find()
+  Product.find()
     .countDocuments()
     .then(numProducts => {
       totalItems = numProducts;
-      Product.find()
-      .skip((page - 1) * ITEMS_PER_PAGE)
-      .limit(ITEMS_PER_PAGE)
+      return Product.find()
+        .skip((page - 1) * ITEMS_PER_PAGE)
+        .limit(ITEMS_PER_PAGE);
     })
-  .then(products => {
+    .then(products => {
       res.render('shop/index', {
         prods: products,
         pageTitle: 'Shop',
         path: '/',
-        totalProducts: totalItems,
+        currentPage: page,
         hasNextPage: ITEMS_PER_PAGE * page < totalItems,
         hasPreviousPage: page > 1,
         nextPage: page + 1,
         previousPage: page - 1,
         lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
       });
-  })
-  .catch(err => {
-    const error = new Error(err);
-    error.httpStatusCode = 500;
-    return next(error);
-  });
+    })
+    .catch(err => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+    });
 };
 
 exports.getCart = (req, res, next) => {
