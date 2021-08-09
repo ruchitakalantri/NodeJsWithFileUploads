35491bcf7fc4fd8e7ef9e0ed4a9d71487c5a1ca8
Ruchita Kalantri <rkalantri.kalantri@yahoo.com>
2021-08-09 13:51:48 +0530

    sending and haneling background request

--- public/js/admin.js
+++ public/js/admin.js
@@ -6,4 +6,18 @@ const deleteProduct = (btn) => {
     console.log('prodId : ' + prodId);
     console.log('csrf token : ' + csrf);
 
+    // send http request 
+    fetch('/admin/product/' + prodId , {
+        method : 'DELETE' ,
+        headers : {
+            'csrf-token' : csrf
+        }
+    })
+    .then(result => {
+        console.log(result);
+    })
+    .catch(err => {
+        console.log(err);
+    });
+
 };
--- routes/admin.js
+++ routes/admin.js
@@ -50,8 +50,10 @@ router.post(
     ],
     isAuth, 
     adminController.
-    postEditProduct);
+    postEditProduct
+);
 
-router.post('/delete-product',isAuth, adminController.postDeleteProduct);
+
+router.delete('/product/:productId' ,isAuth , adminController.deleteProduct);
 
 module.exports = router;
